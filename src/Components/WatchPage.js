cb419d57c0a457095e7b6a584ab51e4fa45f635b
Dikshant <singhdikshant200@gmail.com>
2023-11-03 17:59:36 +0530

    useparam to get video id

--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import {  useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
+
+
+  const [searchParam ] = useSearchParams();
+  console.log(searchParam.get("v"));
+
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
   return (
     <div>
-        watchpage
-      
+      <iframe
+        width="900"
+        height="500"
+        src={"https://www.youtube.com/embed/" + searchParam.get("v")}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default WatchPage
+export default WatchPage;
