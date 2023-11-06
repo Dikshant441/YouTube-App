6adcd66a132328d1a92eae0dae9fc5662161637c
Dikshant <singhdikshant200@gmail.com>
2023-11-06 18:21:31 +0530

    on enter user comment work(on colsole)

--- src/Components/LiveChat.js
+++ src/Components/LiveChat.js
@@ -4,10 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomMessages, generateRandomNames } from "../utils/helper";
 
-
 const LiveChat = () => {
-
-   
+  const [liveMessgae, setLiveMessage] = useState("");
   const dispatch = useDispatch();
 
   const chatMessages = useSelector((store) => store.chat.messages);
@@ -21,29 +19,39 @@ const LiveChat = () => {
           name: generateRandomNames(),
           message: generateRandomMessages(20) + " 🚀",
         })
-      ) 
+      );
     }, 500);
 
     return () => clearInterval(i);
   }, []);
-  return (
 
+  return (
     <>
-    <div className="w-full ml-3 h-[600px] border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-      {
-        // dont use index as a key
-      chatMessages.map((c , i) => (
-        <ChatMessages key={i} name={c.name} message={c.message} />
-      ))}
-    </div>
-    <div className="w-full mx-3 my-2 p-2 border border-black">
+      <div className="w-full ml-3 h-[600px] border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        {
+          // dont use index as a key
+          chatMessages.map((c, i) => (
+            <ChatMessages key={i} name={c.name} message={c.message} />
+          ))
+        }
+      </div>
+      <form
+        className="w-full mx-3 my-2 p-2 border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("on form submit");
+        }}
+      >
         <input
-        className="w-3/4 p-1 border border-black"
-        type ="text"
+          className="w-3/4 p-1 border border-black"
+          type="text"
+          vlaue={liveMessgae}
+          onClick={(e) => {
+            setLiveMessage(e.target.vlaue);
+          }}
         />
         <button className="w-20 mx-4 bg-green-100">send</button>
-
-    </div>
+      </form>
     </>
   );
 };
